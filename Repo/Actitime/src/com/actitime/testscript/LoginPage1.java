13a8ae62feb7c2f25e78744d3dd96d2ee4330312
Balasubramanyam <balumummi@gmail.com>
2024-05-09 17:00:59 +0530

    eclipse t git

---
+++ Repo/Actitime/src/com/actitime/testscript/LoginPage1.java
@@ -0,0 +1,14 @@
+package com.actitime.testscript;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage1 {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/login/");
+		driver.manage().window().minimize();
+		driver.quit();
+	}
+}
